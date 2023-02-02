01db3e22962d689170ee36e61526574cfa0ef830
Aiden <yigwak@gabia.com>
2023-02-02 14:14:28 +0900

    Fix : test config

--- src/test/java/gabia/demo/Service/VotingServiceTest.java
+++ src/test/java/gabia/demo/Service/VotingServiceTest.java
@@ -17,13 +17,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.IntStream;
-
+@ActiveProfiles({"test"})
 @SpringBootTest
 public class VotingServiceTest {
 
