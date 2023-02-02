ca620ff2e7997f3bad51657b8e76bae655fdcce9
Aiden <yigwak@gabia.com>
2023-02-02 15:39:54 +0900

    Fix : test config

--- src/test/java/gabia/demo/Service/VotingServiceTest.java
+++ src/test/java/gabia/demo/Service/VotingServiceTest.java
@@ -24,7 +24,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.IntStream;
-@ActiveProfiles({"test"})
 @SpringBootTest
 public class VotingServiceTest {
 
